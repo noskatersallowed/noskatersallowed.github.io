4b5679e2974fd4370e32b12df1fc41730653f3b0
noskatersallowed <binsiird.tmr@gmail.com>
2024-09-21 23:28:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ function checkPassword() {
 function submitQuiz() {
     // Quiz functionality can be added here
 }
+
+function filterNames() {
+    var input, filter, ul, li, strong, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("brothersList").getElementsByTagName("ul")[0];
+    li = ul.getElementsByTagName("li");
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        strong = li[i].getElementsByTagName("strong")[0]; // Assumes the name is in the first <strong> tag
+        if (strong) {
+            txtValue = strong.textContent || strong.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+}
+
